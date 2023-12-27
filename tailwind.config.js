1d2565e097300d6e7fac500911d9eea34e5abb2a
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-12-27 14:15:11 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "w-primary": "#00668A",
+        "w-secondary": "#004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Poppins, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
