f70662f976f41fa01b381fd256c9da97a3e7f618
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-12-29 01:36:41 +0100

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        "w-primary": "#00668A",
-        "w-secondary": "#004E71",
+        "w-primary": "#5EBBE8",
+        "w-primary-1": "hsl(230, 20%, 17%)",
+        "w-secondary": "#D2CCA1",
+        "w-black-1": "#181818",
+        "w-black-2": "#222222",
+        "w-black-3": "#282828",
+        "w-white-1": "hsl(230, 100%, 95%)",
+        "w-white-2": "hsla(230, 100%, 95%, 0.8)",
+        "w-white-3": "hsla(230, 100%, 95%, 0.6)",
+        // "w-white-1": "#f2f2f2",
+        // "w-white-2": "#f7f7f7",
+        // "w-white-3": "rgba(235, 235, 235, 0.64)",
+        "divider-1": "rgba(84, 84, 84, 0.65)",
+        "divider-2": "rgba(84, 84, 84, 0.48)",
+        "body-bg": "hsl(230, 40%, 12%)",
       },
     },
     fontFamily: {
-      Roboto: ["Poppins, sans-serif"],
+      Roboto: ["'Poppins', sans-serif"],
     },
     container: {
       padding: "2rem",
