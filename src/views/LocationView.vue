462f90e6676aa54e1a1a1734eae4753561f40984
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-12-30 23:25:50 +0100

    feat: adds location view component t

---
+++ src/views/LocationView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AsyncLocationView from "@/components/UI/ViewLocation/AsyncLocationView.vue";
+import LocationViewSkeleton from "@/components/UI/ViewLocation/LocationViewSkeleton.vue";
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <AsyncLocationView />
+
+      <template #fallback>
+        <div class="container">
+          <LocationViewSkeleton />
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<style scoped></style>
